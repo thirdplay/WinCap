5571dc210b0c6a11c0e4baf77651c391940f70ea
thirdplay <chronokreuz@gmail.com>
2015-11-01 07:49:16 +0900

    追加漏れファイルの追加

---
+++ WinCap/Components/ResidentIcon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+
+namespace WinCap.Components
+{
+    /// <summary>
+    /// 常駐アイコン
+    /// </summary>
+    public sealed class ResidentIcon : IDisposable
+    {
+        /// <summary>
+        /// 通知アイコン
+        /// </summary>
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+
+        /// <summary>
+        /// コンテキストメニュー
+        /// </summary>
+        public System.Windows.Forms.ContextMenuStrip ContextMenuStrip
+        {
+            get
+            {
+                return this.notifyIcon.ContextMenuStrip;
+            }
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="iconUri">アイコンURI</param>
+        /// <param name="text">常駐アイコンのテキスト</param>
+        /// <param name="visible">初期表示状態</param>
+        public ResidentIcon(string iconUri, string text, bool visible = true)
+        {
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon();
+            this.notifyIcon.Icon = new Icon(System.Windows.Application.GetResourceStream(new Uri(iconUri)).Stream);
+            this.notifyIcon.Visible = visible;
+            this.notifyIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+        }
+
+        /// <summary>
+        /// このインスタンスによって使用されているリソースを全て破棄する。
+        /// </summary>
+        public void Dispose()
+        {
+            this.notifyIcon.Dispose();
+        }
+    }
+}
---
+++ WinCap/Services/CaptureService.cs
@@ -0,0 +1,125 @@
+﻿using Livet;
+using System;
+using System.Collections.Generic;
+using WinCap.Utilities.Lifetime;
+
+namespace WinCap.Services
+{
+    /// <summary>
+    /// キャプチャサービスの状態を示す識別子
+    /// </summary>
+    public enum CaptureServiceStatus
+    {
+        /// <summary>
+        /// 待機
+        /// </summary>
+        Wait,
+
+        /// <summary>
+        /// キャプチャ中
+        /// </summary>
+        UnderCapture,
+    }
+
+    /// <summary>
+    /// キャプチャサービス
+    /// </summary>
+    public sealed class CaptureService : NotificationObject, IDisposableHolder
+    {
+        #region フィールド
+        /// <summary>
+        /// 基本CompositeDisposable
+        /// </summary>
+        private readonly LivetCompositeDisposable compositeDisposable = new LivetCompositeDisposable();
+
+        /// <summary>
+        /// 現在のキャプチャサービスの状態
+        /// </summary>
+        private CaptureServiceStatus currentSatus = (CaptureServiceStatus)(-1);
+        #endregion
+
+        #region プロパティ
+        /// <summary>
+        /// 現在のサービスを取得する。
+        /// </summary>
+        public static CaptureService Current { get; } = new CaptureService();
+
+        /// <summary>
+        /// キャプチャサービスの状態
+        /// </summary>
+        public CaptureServiceStatus Status
+        {
+            get { return this.currentSatus; }
+            set
+            {
+                if (this.currentSatus != value)
+                {
+                    this.currentSatus = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        private CaptureService() { }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public void Initialize()
+        {
+        }
+
+        /// <summary>
+        /// 画面全体をキャプチャする。
+        /// </summary>
+        public void CaptureScreen()
+        {
+            /*
+                : [一連の流れ]
+                : Service：状態確認 ⇒ 待機状態以外は処理終了
+                : Service：キャプチャ生成
+                : Service：共通設定の反映
+                :
+                : Service：キャプチャ処理の呼び出し
+                : ?      ：画像出力
+            */
+        }
+
+        /// <summary>
+        /// 選択コントロールをキャプチャする。
+        /// </summary>
+        public void CaptureControl()
+        {
+            /*
+                : [一連の流れ]
+                : Service：状態確認 ⇒ 待機状態以外は処理終了
+                :
+                : ?      ：コントロール選択
+                :            
+                : Service：キャプチャ生成
+                : Service：共通設定の反映
+                :
+                : Service：キャプチャ処理の呼び出し
+                : ?      ：画像出力
+                :
+                : ※コントロール選択処理をキャプチャに寄せるorサービスとの間に機能をかますか検討。
+            */
+        }
+
+        #region IDisposableHoloder members
+        ICollection<IDisposable> IDisposableHolder.CompositeDisposable => this.compositeDisposable;
+
+        /// <summary>
+        /// このインスタンスによって使用されているリソースを全て破棄する。
+        /// </summary>
+        public void Dispose()
+        {
+            this.compositeDisposable.Dispose();
+        }
+        #endregion
+    }
+}
