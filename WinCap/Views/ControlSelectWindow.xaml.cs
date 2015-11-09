4d8201a2b493972bbb2ee70fbf350908c6463c56
thirdplay <chronokreuz@gmail.com>
2015-11-10 03:09:54 +0900

    リネーム

---
+++ WinCap/Views/ControlSelectWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Livet;
+using System;
+using System.Collections.Generic;
+using WinCap.Utilities.Lifetime;
+
+namespace WinCap.Views
+{
+    /// <summary>
+    /// ControlSelectWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ControlSelectWindow
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ControlSelectWindow()
+        {
+            InitializeComponent();
+            this.MouseDown += Button_Click;
+        }
+
+        // TODO:実装時に下記をビヘイビアへ移動する
+        // ※EventTrigger
+        public event EventHandler Selected = null;
+        private void Button_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Selected?.Invoke(sender, e);
+        }
+    }
+}
