f9c67cebc60e4510ea9f1c84b0fe3e94c449b736
thirdplay <chronokreuz@gmail.com>
2015-11-29 23:35:58 +0900

    非表示ウィンドウの分離、仮追加

---
+++ source/WinCap/Views/InvisibleWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MetroRadiance.Controls;
+
+namespace WinCap.Views
+{
+    /// <summary>
+    /// InvisibleWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class InvisibleWindow
+    {
+        public InvisibleWindow()
+        {
+            InitializeComponent();
+            this.Loaded += (sender, e) => this.Hide();
+        }
+    }
+}
