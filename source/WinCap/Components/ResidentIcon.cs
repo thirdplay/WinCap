35b4cb0b739c1410084aa9e709735e1afd639eae
thirdplay <chronokreuz@gmail.com>
2015-12-06 00:30:35 +0900

    常駐アイコンの不具合修正

--- source/WinCap/Components/ResidentIcon.cs
+++ source/WinCap/Components/ResidentIcon.cs
@@ -29,11 +29,13 @@ namespace WinCap.Components
         /// </summary>
         /// <param name="iconUri">アイコンURI</param>
         /// <param name="text">常駐アイコンのテキスト</param>
-        public ResidentIcon(string iconUri, string text)
+        /// <param name="visible">表示状態</param>
+        public ResidentIcon(string iconUri, string text, bool visible = true)
         {
             this.notifyIcon = new System.Windows.Forms.NotifyIcon();
             this.notifyIcon.Icon = new Icon(System.Windows.Application.GetResourceStream(new Uri(iconUri)).Stream);
             this.notifyIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            this.notifyIcon.Visible = visible;
         }
 
         /// <summary>
