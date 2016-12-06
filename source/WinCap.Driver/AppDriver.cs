e8e6b5c0fb384f37009e903818bd46b052fcf9c2
thirdplay <thirdplay@outlook.com>
2016-12-07 00:24:20 +0900

    アプリケーションドライバの作成中

--- source/WinCap.Driver/AppDriver.cs
+++ source/WinCap.Driver/AppDriver.cs
@@ -1,9 +1,42 @@
-﻿namespace WinCap.Driver
+﻿using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace WinCap.Driver
 {
     /// <summary>
     /// 対象アプリケーションを操作する機能を提供します。
     /// </summary>
     public class AppDriver
     {
+        /// <summary>
+        /// Windowsアプリケーション操作クラス。
+        /// </summary>
+        private WindowsAppFriend _app;
+
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        public AppDriver()
+        {
+#if DEBUG
+            var build = "Debug";
+#else
+            var build = "Release";
+#endif
+            var exePath = Path.GetFullPath("../../../../WinCap/bin/x86/" + build + "/WinCap.exe");
+            this._app = new WindowsAppFriend(Process.Start(exePath, "-ShowSettings"));
+        }
+
+        /// <summary>
+        /// リソースを解放します。
+        /// </summary>
+        public void Release()
+        {
+            //this._app.Dispose();
+            this._app.Type<Application>().Current.Shutdown();
+        }
     }
 }
