e4486197eb717b6f7a4df617c0d7641131a7b5ba
thirdplay <thirdplay@outlook.com>
2016-12-09 00:10:55 +0900

    アプリケーションドライバの作成中

--- source/WinCap.Driver/SettingsWindowDriver.cs
+++ source/WinCap.Driver/SettingsWindowDriver.cs
@@ -1,4 +1,5 @@
 ﻿using Codeer.Friendly.Windows.Grasp;
+using RM.Friendly.WPFStandardControls;
 
 namespace WinCap.Driver
 {
@@ -13,12 +14,19 @@ namespace WinCap.Driver
         public WindowControl Window { get; private set; }
 
         /// <summary>
+        /// スクロールの遅延時間を取得します。
+        /// </summary>
+        public WPFTextBox ScrollDelayTime { get; private set; }
+
+        /// <summary>
         /// コンストラクタ。
         /// </summary>
         /// <param name="windowControl">ウィンドウコントロール</param>
         public SettingsWindowDriver(WindowControl windowControl)
         {
+            var visualTree = windowControl.VisualTree();
             this.Window = windowControl;
+            this.ScrollDelayTime = new WPFTextBox(visualTree.ByBinding("ScrollDelayTime").Single());
         }
     }
 }
