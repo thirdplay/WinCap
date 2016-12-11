1ae21e8cc69313e474b6442839700e4621f5b942
thirdplay <thirdplay@outlook.com>
2016-12-11 10:52:28 +0900

    アプリケーションドライバの作成中

--- source/WinCap.Driver/SettingsWindowDriver.cs
+++ source/WinCap.Driver/SettingsWindowDriver.cs
@@ -14,6 +14,27 @@ namespace WinCap.Driver
         public WindowControl Window { get; private set; }
 
         /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        /// <param name="windowControl">ウィンドウコントロール</param>
+        public SettingsWindowDriver(WindowControl windowControl)
+        {
+            var visualTree = windowControl.VisualTree();
+            this.Window = windowControl;
+        }
+    }
+
+    /// <summary>
+    /// 設定ウィンドウの全般タブを操作する機能を提供します。
+    /// </summary>
+    public class General
+    {
+        /// <summary>
+        /// ウィンドウコントロールを取得します。
+        /// </summary>
+        public WindowControl Window { get; private set; }
+
+        /// <summary>
         /// スクロールの遅延時間を取得します。
         /// </summary>
         public WPFTextBox ScrollDelayTime { get; private set; }
@@ -22,10 +43,15 @@ namespace WinCap.Driver
         /// コンストラクタ。
         /// </summary>
         /// <param name="windowControl">ウィンドウコントロール</param>
-        public SettingsWindowDriver(WindowControl windowControl)
+        public General(WindowControl windowControl)
         {
             var visualTree = windowControl.VisualTree();
+            var logicalTree = windowControl.LogicalTree();
+
             this.Window = windowControl;
+            var listBox = new WPFListBox(logicalTree.ByType("MetroRadiance.UI.Controls.TabView").ByBinding("TabItems").Single());
+
+            listBox.EmulateChangeSelectedIndex(1);
             this.ScrollDelayTime = new WPFTextBox(visualTree.ByBinding("ScrollDelayTime").Single());
         }
     }
